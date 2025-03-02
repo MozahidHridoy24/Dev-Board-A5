761cc001b88e968d6115017f1ce5cb013599298a
mozahidhridoy24 <hridoy1407@gmail.com>
2025-03-02 18:14:54 +0600

    added button function

--- script/index.js
+++ script/index.js
@@ -1,3 +1,8 @@
+const completedTask = document.getElementById("completed-task");
+let totalTask = parseInt(completedTask.innerText)
+const assignedTask = document.getElementById("assigned-task");
+let remainingTask = parseInt(assignedTask.innerText);
+
 function changeBgColor() {
     const randomColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
     document.body.style.backgroundColor = randomColor;
@@ -9,6 +14,33 @@ document.getElementById("discover").
     })
 
 document.getElementById("color-btn").
-    addEventListener("click", function(){
+    addEventListener("click", function () {
         changeBgColor();
     })
+
+
+const buttons = document.querySelectorAll(".complete-card-button")
+for (const button of buttons) {
+    button.addEventListener("click", function () {
+        alert('Board Updated Successfully');
+        totalTask++;
+        remainingTask--;
+        console.log(totalTask, remainingTask)
+
+        completedTask.innerText = totalTask;
+        assignedTask.innerText = remainingTask;
+
+        const historyDiv = document.getElementById("history");
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 PM";
+        p.style.marginBottom ="10px" ;
+       historyDiv.appendChild(p);
+
+        this.disabled = true;
+    })
+}
+
+const today = new Date();
+const formattedDate = today.toDateString();
+document.getElementById("todays-date").innerText =formattedDate;
+
